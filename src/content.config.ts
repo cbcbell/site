21fd71eac4558b0ae385dfd23721802dfa457621
Lachlan Campbell <lachlan@watershedclimate.com>
2025-12-26 02:11:40 -0500

    Try resolving in GalleryLink

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,4 @@
-import type { ImageMetadata } from "astro"
 import { defineCollection, z } from "astro:content"
-
-const coverImages = import.meta.glob<{ default: ImageMetadata }>(
-  "./assets/sync/gallery-covers/*.{jpg,jpeg,png}",
-  { eager: true },
-)
-
-const coverImageMap = Object.entries(coverImages).reduce<
-  Record<string, ImageMetadata>
->((acc, [path, mod]) => {
-  const filename = path.split("/").pop()
-  const [slug] = filename?.split(".") ?? []
-  if (slug) acc[slug] = mod.default
-  return acc
-}, {})
 
 const newsletters = defineCollection({
   type: "content",
@@ -40,12 +25,12 @@ const galleries = defineCollection({
       })
       .transform((data) => {
         const coverSlug = `${data.type}-${data.typeSlug}`
-        const cover = coverImageMap[coverSlug]
 
         return {
           ...data,
-          cover,
-          coverUrl: `https://s3.us-west-001.backblazeb2.com/cbcampbell-com/gallery-covers/${data.type}-${data.typeSlug}.jpg`,
+          cover: undefined,
+          coverSlug,
+          coverUrl: `https://s3.us-west-001.backblazeb2.com/cbcampbell-com/gallery-covers/${coverSlug}.jpg`,
         }
       }),
 })
